0e504e2d355c502d3db1b4c925f3df4103045a89
최영원 <chywon210@naver.com>
2024-05-01 21:52:27 +0900

    제이크가 마스터한테 0952

--- app/src/main/java/com/example/peojulgae/MainActivity.java
+++ app/src/main/java/com/example/peojulgae/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-// 제이크가 마스터한테 0951
+// 제이크가 마스터한테 0952
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
