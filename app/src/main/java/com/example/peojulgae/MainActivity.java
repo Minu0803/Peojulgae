ef86681621f06632e8f14001d2bf143b3099051e
최영원 <chywon210@naver.com>
2024-05-01 23:26:10 +0900

    1126

--- app/src/main/java/com/example/peojulgae/MainActivity.java
+++ app/src/main/java/com/example/peojulgae/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-// 1125
+// 1126
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
