07f10b11fe54ffa36a57ffc0854fb0f1368d1d7c
최영원 <chywon210@naver.com>
2024-05-01 22:34:05 +0900

    제이크 10/33

--- app/src/main/java/com/example/peojulgae/MainActivity.java
+++ app/src/main/java/com/example/peojulgae/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-//제이크가 미누한테1
+// 제이크 10/33
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
