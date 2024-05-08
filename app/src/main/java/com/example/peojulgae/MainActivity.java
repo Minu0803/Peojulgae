a3d6db39eb9e0599ec75e31d1649a382ea7190ce
Minuffy <128578002+Minu0803@users.noreply.github.com>
2024-05-08 22:59:14 +0900

    5/8일 피드백 중 실행가능되는 파일 첫 커밋

--- app/src/main/java/com/example/peojulgae/MainActivity.java
+++ app/src/main/java/com/example/peojulgae/MainActivity.java
@@ -8,18 +8,83 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import fragment.Frag1;
+import fragment.Frag2;
+import fragment.Frag3;
+import fragment.Frag4;
 public class MainActivity extends AppCompatActivity {
-// 12/22
+
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Frag1 frag1;
+    private Frag2 frag2;
+    private Frag3 frag3;
+    private Frag4 frag4;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnItemSelectedListener(menuItem -> {
+            int itemId = menuItem.getItemId();
+            if (itemId == R.id.fragment_home) {
+                setFrag(0);
+            } else if (itemId == R.id.fragment_category) {
+                setFrag(1);
+            } else if (itemId == R.id.fragment_mart) {
+                setFrag(2);
+            } else if (itemId == R.id.fragment_mypage) {
+                setFrag(3);
+            }
+            return false;
         });
+        frag1 = new Frag1();
+        frag2 = new Frag2();
+        frag3 = new Frag3();
+        frag4 = new Frag4();
+        setFrag(0); // 첫 프래그먼트 화면 선택
+
+    }
+
+
+    //프래그먼트 교체 실행문
+    private void setFrag(int n) {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch (n) {
+            case 0:
+                ft.replace(R.id.main_frame, frag1);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, frag2);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame, frag3);
+                ft.commit();
+                break;
+            case 3:
+                ft.replace(R.id.main_frame, frag4);
+                ft.commit();
+                break;
+
+        }
     }
 }
---
+++ app/src/main/java/fragment/Frag2.java
@@ -0,0 +1,25 @@
+package fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+import com.example.peojulgae.R;
+
+public class Frag2 extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.categoryfragment,container,false);
+        return view;
+    }
+}
+
