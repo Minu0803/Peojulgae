1a7eec8a0c8882dfdcb796c66ae8e96bd6d302c2
Minuffy <128578002+Minu0803@users.noreply.github.com>
2024-05-08 12:15:03 +0900

    first commit

--- app/src/main/java/com/example/peojulgae/MainActivity.java
+++ app/src/main/java/com/example/peojulgae/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-// 1142
+// 0508
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
