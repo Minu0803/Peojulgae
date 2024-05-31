84f1d8bdeb92ae1b7e4b999fe231e97a39346e98
sleepyeon <030703kimsy@naver.com>
2024-05-31 14:14:54 +0900

    비밀~

--- app/src/main/java/com/example/peojulgae/BaloonActivity.java
+++ app/src/main/java/com/example/peojulgae/BaloonActivity.java
@@ -8,6 +8,6 @@ public class BaloonActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.food_display);
+        setContentView(R.layout.food_list);
     }
 }
