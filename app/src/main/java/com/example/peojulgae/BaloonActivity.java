9738d393a5abd2dd3b0ddc6e732f7da6ab3839e2
Minuffy <128578002+Minu0803@users.noreply.github.com>
2024-06-02 20:19:10 +0900

    카테고리 화면 수정 및 지도 화면 동작 업데이트

--- app/src/main/java/com/example/peojulgae/BaloonActivity.java
+++ app/src/main/java/com/example/peojulgae/BaloonActivity.java
@@ -8,6 +8,6 @@ public class BaloonActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.food_list);
+        setContentView(R.layout.store_display);
     }
 }
