50345734b65a8ddf55efc22b27c8d4e2e0ad2c0a
Minuffy <128578002+Minu0803@users.noreply.github.com>
2024-06-02 20:45:32 +0900

    Store to Food 화면 동작 추가

--- app/src/main/java/com/example/peojulgae/BaloonActivity.java
+++ app/src/main/java/com/example/peojulgae/BaloonActivity.java
@@ -1,13 +1,31 @@
 package com.example.peojulgae;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class BaloonActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.store_display);
+
+
+        CardView cardView = findViewById(R.id.CardView01); // 카드뷰 찾기
+
+
+        cardView.setOnClickListener(new View.OnClickListener() {  // 카드뷰 클릭 리스너 설정
+            @Override
+            public void onClick(View v) {
+                // FoodListActivity 시작
+                Intent intent = new Intent(BaloonActivity.this, StoretoFood.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
---
+++ app/src/main/java/com/example/peojulgae/StoretoFood.java
@@ -0,0 +1,16 @@
+package com.example.peojulgae;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class StoretoFood extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food_list);
+
+    }
+}
