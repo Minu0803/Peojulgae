e1bab9a3d2fd138d42084b4a3d5546d283ea867c
Minuffy <128578002+Minu0803@users.noreply.github.com>
2024-06-02 21:33:13 +0900

    결제하기 , 포장하기 버튼 구현

--- app/src/main/java/com/example/peojulgae/FoodListActivity.java
+++ app/src/main/java/com/example/peojulgae/FoodListActivity.java
@@ -1,6 +1,9 @@
 package com.example.peojulgae;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FoodListActivity extends AppCompatActivity {
@@ -8,5 +11,28 @@ public class FoodListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_list);
+
+        // food_lbutton04 버튼 찾기
+        Button TakePayButton = findViewById(R.id.food_lbutton04);
+        Button HavePayButton = findViewById(R.id.food_lbutton05);
+
+        // food_lbutton04 버튼 클릭 리스너 설정
+        TakePayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TakePayActivity 시작
+                Intent intent1 = new Intent(FoodListActivity.this, TakePayActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+        HavePayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TakePayActivity 시작
+                Intent intent2 = new Intent(FoodListActivity.this, HavePayActivity.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/peojulgae/HavePayActivity.java
+++ app/src/main/java/com/example/peojulgae/HavePayActivity.java
@@ -3,10 +3,10 @@ package com.example.peojulgae;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class TakePayActivity extends AppCompatActivity {
+public class HavePayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.take_pay);
+        setContentView(R.layout.have_pay);
     }
 }
