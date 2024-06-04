aaccf138530b463c33a93778bff051783817aaf2
Minuffy <128578002+Minu0803@users.noreply.github.com>
2024-06-04 22:11:04 +0900

    마트 기능 완성

---
+++ app/src/main/java/com/example/peojulgae/FoodCategoryActivity.java
@@ -0,0 +1,12 @@
+package com.example.peojulgae;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FoodCategoryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food_category);
+    }
+}
--- app/src/main/java/fragment/Frag3.java
+++ app/src/main/java/fragment/Frag3.java
@@ -1,14 +1,16 @@
 package fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.peojulgae.FoodCategoryActivity;
 import com.example.peojulgae.R;
 
 public class Frag3 extends Fragment {
@@ -17,7 +19,34 @@ public class Frag3 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.activity_grocery_store,container,false);
+        view = inflater.inflate(R.layout.activity_grocery_store, container, false);
+
+        ImageView groceryStoreImg = view.findViewById(R.id.grocery_store_img);
+        ImageView groceryStoreImg2 = view.findViewById(R.id.grocery_store_img02);
+        ImageView groceryStoreImg3 = view.findViewById(R.id.grocery_store_img03);
+        groceryStoreImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FoodCategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        groceryStoreImg2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FoodCategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        groceryStoreImg3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FoodCategoryActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
