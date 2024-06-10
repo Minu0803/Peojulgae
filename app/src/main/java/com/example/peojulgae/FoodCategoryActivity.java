274c45b200bb542de0b87234c463229531991d7e
Minuffy <128578002+Minu0803@users.noreply.github.com>
2024-06-10 14:25:58 +0900

    마트 제품 상세보기 기능 추가

--- app/src/main/java/com/example/peojulgae/FoodCategoryActivity.java
+++ app/src/main/java/com/example/peojulgae/FoodCategoryActivity.java
@@ -1,6 +1,9 @@
 package com.example.peojulgae;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FoodCategoryActivity extends AppCompatActivity {
@@ -8,5 +11,14 @@ public class FoodCategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_category);
+
+        ImageView peachImageView = findViewById(R.id.peachimage);
+        peachImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FoodCategoryActivity.this, MartSpecific.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/peojulgae/MartSpecific.java
@@ -0,0 +1,12 @@
+package com.example.peojulgae;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MartSpecific extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.martspecific);
+    }
+}
